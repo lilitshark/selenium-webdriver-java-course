412cb939f40f07abe1f5eedd507e5538bcdb96aa
Lilit Shark <lilit.sharkhatunyan@gmail.com>
2020-04-29 15:53:55 +0200

    Chapter 13: Event Listeners. Added EventReporter class in utils that will implement WebDriverEventListener interface

--- test/java/base/BaseTests.java
+++ test/java/base/BaseTests.java
@@ -3,31 +3,27 @@ package base;
 import com.google.common.io.Files;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.HomePage;
-import untils.WindowManager;
-
+import utils.EventReporter;
+import utils.WindowManager;
 import java.io.File;
 import java.io.IOException;
-import java.sql.SQLOutput;
-import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+
 
 public class BaseTests {
-
-    private WebDriver driver;
+    private EventFiringWebDriver driver;
     protected HomePage homePage;
 
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
         //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         //driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         //driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
